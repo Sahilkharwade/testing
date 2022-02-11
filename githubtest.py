c4d1b9f679fc83c2c34142f78ce07c153568b071
Sahilkharwade <63150574+Sahilkharwade@users.noreply.github.com>
2022-02-11 12:10:58 +0530

    Update githubtest.py

--- githubtest.py
+++ githubtest.py
@@ -4,7 +4,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "yourusername",
-    password = "your_password"
+    password = "checking@123"
 )
  
 # Printing the connection object
